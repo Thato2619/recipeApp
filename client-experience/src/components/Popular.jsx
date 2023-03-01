09f14a4653f365e56320ea58f3d35788d24736a6
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-03-01 21:59:59 +0200

    install react icons

--- client-experience/src/components/Popular.jsx
+++ client-experience/src/components/Popular.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
 
 function Popular() {
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        getPopular();
+    },[]);
+
+    //fetch api, everything fetches everything every popular ecioe from the api 
+
+    const getPopular = async () => {
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&NUMBER=9`
+          );
+        const data = await api.json();
+        setPopular(data.recipes)
+
+    }
   return (
     <div>Popular</div>
   )
