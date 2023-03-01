c8b4bfa3ed6bd31ba4fafc970302e04087ead7b5
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-03-01 14:16:38 +0200

    get link from font awesome
    - change background colour of navbar to green
    - change the texts color to a lime green
    - get search bar icon for search bar

--- client-experience/src/navbar/Navbar.jsx
+++ client-experience/src/navbar/Navbar.jsx
@@ -3,9 +3,20 @@ import './navbar.css';
 export default function Navbar() {
   return (
     <div className='nav-top'>
-        <div className="nav-left"></div>
-        <div className="nav-centre"></div>
-        <div className="nav-right"></div>
+        <div className="nav-left">
+            <h1>WeKnowYouHungry</h1>
+        </div>
+        <div className="nav-centre">
+        <i classname="searchBar fa-light fa-magnifying-glass"></i>
+        </div>
+        <div className="nav-right">
+            <ul className='nav-list'>
+                <li className='nav-list-items'>HOME</li>
+                <li className='nav-list-items'>CONTACT</li>
+                <li className='nav-list-items'>WRITE</li>
+                <li className='nav-list-items'>LOGOUT</li>
+            </ul>
+        </div>
     </div>
   )
 }
